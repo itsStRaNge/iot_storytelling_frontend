bcaed70c633bc92827588f52e5e697f16f56b19d
Øyvind Sørdal Klungre <oklunger@online.no>
2018-11-02 10:56:36 +0100

    Added function in comments

--- app/src/main/java/edu/ntnu/iot_storytelling_sensor/SensorUtilities.java
+++ app/src/main/java/edu/ntnu/iot_storytelling_sensor/SensorUtilities.java
@@ -14,7 +14,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-\\import android.graphics.ImageDecoder;
+//import android.graphics.ImageDecoder;
 
 import org.json.JSONException;
 import org.json.JSONObject;
